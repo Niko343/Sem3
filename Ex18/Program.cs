46c16bf73c892c55a362193dd39801f1d5f75d8b
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-25 11:21:45 +0300

    18

---
+++ Ex18/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int num = int.Parse(Console.ReadLine());
+if (num == 1)
+{
+    Console.WriteLine("Координата X > 0, координата y > 0");
+}
+else if (num == 2)
+{
+    Console.WriteLine("Координата X < 0, координата y > 0");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Координата X < 0, координата y < 0");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Координата X > 0, координата y < 0");
+}
+else
+{
+    Console.WriteLine("Нет такой четверти");
+}
