053c829747b47f62c55033c3b49786700c739bac
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-25 11:05:31 +0300

    Ex17

---
+++ ex17/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите координату X: ");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X == 0 || Y == 0)
+{
+    Console.WriteLine("Координаты не соответствуют конкретной четверти");
+}
+else if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Координаты принадлежат 1 четверти");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Координаты принадлежат 2 четверти");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Координаты принадлежат 3 четверти");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Координаты принадлежат 4 четверти");
+}
