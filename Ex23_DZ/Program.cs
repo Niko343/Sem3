21fc292d98e8a17057abf5998f8935ba5fa93d36
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-25 12:38:35 +0300

    Finish

---
+++ Ex23_DZ/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int index = 1;
+while (index <= N)
+{
+    Console.Write(Math.Pow(index, 3));
+    Console.Write(", ");
+    index++;   
+}
