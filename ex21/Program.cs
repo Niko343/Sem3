e19255f937b1b954030f123e4a9f5ee1e0aa84d6
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-25 11:49:57 +0300

    21

---
+++ ex21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первую координату X: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первую координату Y: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую координату X: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую координату Y: ");
+int y2 = int.Parse(Console.ReadLine());
+double a = Math.Pow(x1 - x2, 2);
+double b = Math.Pow(y1 - y2, 2);
+double c = a + b;
+double S = Math.Round(Math.Sqrt(c), 2);
+Console.Write("Расстояние между точками = ");
+Console.WriteLine(S);
+
+
